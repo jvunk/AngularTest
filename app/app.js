a0005e21e4c9b8344aabf753770150704920c6d4
jvunk <jon.vunk@gmail.com>
2016-02-12 08:04:11 -0600

    Example of shared word and excel doc

--- app/app.js
+++ app/app.js
@@ -23,6 +23,118 @@ angular.module('app', ['addItem', 'siteGlobals'])
         });
 })
 
-.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+.controller('AppCtrl', function ($scope, $sce) {
+    ctrl = this;
+    ctrl.files = [
+        {
+            name: "Shared Excel",
+            id: "",
+            url: "https://docs.google.com/spreadsheets/d/12Qao5hKJXdyfB7nRhrfhLpO5tppbopgql1MhCHFMpLA/edit#gid=0"
+        },
+    {
+        name: "Shared Word",
+        id: "",
+        url: "https://docs.google.com/document/d/166P_9P9RmlCuhhrPA6I2Ufgq7bTSoib81feZbrCUSdM/edit"
+    }
+    ];
+
+    // Your Client ID can be retrieved from your project in the Google
+    // Developer Console, https://console.developers.google.com
+    var CLIENT_ID = '424718691911-ceetvtocnuab40q0ss2rf0ti2p9o74l4.apps.googleusercontent.com';
+
+    var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+
+    //on Load
+    //checkAuth();
+
+    /**
+     * Check if current user has authorized this application.
+     */
+    function checkAuth() {
+        console.log('1');
+        gapi.auth.authorize(
+          {
+              'client_id': CLIENT_ID,
+              'scope': SCOPES.join(' '),
+              'immediate': true
+          }, handleAuthResult);
+    }
+
+    /**
+     * Handle response from authorization server.
+     *
+     * @param {Object} authResult Authorization result.
+     */
+    function handleAuthResult(authResult) {
+        console.log('2');
+        var authorizeDiv = document.getElementById('authorize-div');
+        if (authResult && !authResult.error) {
+            // Hide auth UI, then load client library.
+            authorizeDiv.style.display = 'none';
+            loadDriveApi();
+        } else {
+            // Show auth UI, allowing the user to initiate authorization by
+            // clicking authorize button.
+            authorizeDiv.style.display = 'inline';
+        }
+    }
+
+    /**
+     * Initiate auth flow in response to user clicking authorize button.
+     *
+     * @param {Event} event Button click event.
+     */
+    ctrl.handleAuthClick = function (event) {
+        console.log('3');
+        gapi.auth.authorize(
+          { client_id: CLIENT_ID, scope: SCOPES, immediate: false },
+          handleAuthResult);
+        return false;
+    }
+
+    /**
+     * Load Drive API client library.
+     */
+    function loadDriveApi() {
+        console.log('4');
+        gapi.client.load('drive', 'v3', listFiles);
+    }
+
+    /**
+     * Print files.
+     */
+    function listFiles() {
+
+        console.log('5');
+
+        var request = gapi.client.drive.files.list({
+            'pageSize': 10,
+            'fields': "nextPageToken, files(id, name)"
+        });
+
+        request.execute(function (resp) {
+
+            angular.forEach(resp.files, function (file, key) {
+                ctrl.files.push({
+                    name: file.name,
+                    id: file.id,
+                    url: "https://docs.google.com/document/d/" + file.id
+                })
+            });
+
+            $scope.$digest();
+        });
+    }
+
+    ctrl.selectFile = function (file) {
+
+        ctrl.selectedFile = file;
+        $(".iFrameHolder").find('iframe').remove();
+        $('<iframe >', {
+            src: file.url,
+            id: 'myFrame',
+            frameborder: 0
+        }).appendTo('.iFrameHolder');
+    }
 
 });
